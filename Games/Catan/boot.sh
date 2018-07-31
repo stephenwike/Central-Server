aa80f83e532f19244693863cc509b999564c1f32
stephenwike <stephen.c.wise@gmail.com>
2018-07-30 19:34:27 -0600

    attempt to boot website from shell script

---
+++ Games/Catan/boot.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+matchbox-window-manager -use_cursor no&
+chromium-browser --incognito file:///gameCentralWebKiosk/Central-Server/Games/Catan/index.html
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-//const hbs = require('express-handlebars');
+var shell = require('shelljs');
 var gc_app = express(); // GAME CENTRAL APP
 var gc_app_port = 9090;
 var gc_server = require('http').Server(gc_app);
@@ -17,13 +17,35 @@ gc_app.get('/', (req, res) => {
   res.send("Welcome to socket");
   //res.sendFile(path.join(ROOT, GAME, 'HOME/index.html'));
 });
-gc_io.on('connection', (client) => { 
-  console.log("CONNECTION IO"); 
 
-  gc_io.emit('welcome', { hello: "world" });
+gc_io.on('connection', (socket) => {	
+
+  console.log("CONNECTION IO"); 
+  console.log("ID: " + socket.id);
+  
+  socket.on("RUN_catan", (msg) =>
+  {
+	console.log("running catan... broadcast load config");
+	socket.broadcast.emit("LOADING_config");
+	
+	var htmlpath = path.join(__dirname, '/Games/Catan/boot.sh');
+	console.log("HTML path: " + htmlpath);
+	if (shell.exec("xinit " + htmlpath).code !== 0)
+	{
+		shell.echo('Error: Git commit failed');
+		shell.exit(1);
+	}
+  });
+  
+  //gc_io.emit('welcome', { hello: "world" });
   //socket.on('buhbye', (data) => {
   //  console.log(data);
   //});
+});
+
+gc_io.on('RUN_catan', (client) => {
+	console.log("HERE");
+	console.log("Client id: " + client.id + " has run catan application.");
 });
 
 gc_server.listen(gc_app_port, function() {
