9d68cbaab4c99c434283a9f94b70260f908827fc
stephenwike <stephen.c.wise@gmail.com>
2018-08-02 02:47:28 -0600

    bug fix 6

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 var express = require('express');
 var path = require('path');
 var shell = require('shelljs');
-var gc_app = express(); // GAME CENTRAL APP
+var tv_app = express(); // GAME CENTRAL APP
 var an_app = express(); // ANDROID APP
-var gc_app_port = 9090;
+var tv_app_port = 9090;
 var an_app_port = 7070;
-var gc_server = require('http').Server(gc_app);
+var tv_server = require('http').Server(tv_app);
 var an_server = require('http').Server(an_app);
-var gc_io = require('socket.io')(gc_server);
+var tv_io = require('socket.io')(tv_server);
 var an_io = require('socket.io')(an_server);
 
-var gc_socket;
+var tv_socket;
 
 const ROOT = "/var/www/";
 const GAME = "GameCentral/";
 
 // Middleware
-//gc_app.use(express.static(path.join(ROOT, GAME, 'HOME')));
+//tv_app.use(express.static(path.join(ROOT, GAME, 'HOME')));
 
-gc_app.get('/', (req, res) => {
+tv_app.get('/', (req, res) => {
   console.log("Connected from TV");
   res.sendFile(__dirname + '/Games/TestChat/index.html');
 });
@@ -27,13 +27,14 @@ an_app.get('/', (req, res) => {
   console.log("Connected from ANDROID");
 });
 
-gc_io.on('connection', (socket) => {
-  console.log("CONNECTION TO PORT: " + gc_app_port + ", USER: " + socket.id); 
-  gc_socket = socket;
+tv_io.on('connection', (socket) => {
+  console.log("CONNECTION TO PORT: " + tv_app_port + ", USER: " + socket.id); 
+  tv_socket = socket;
+  console.log("tv socket: " + socket);
   
   socket.on('disconnect', () => 
   {
-	console.log("USER: " + socket.id + " disconnected from PORT: " + gc_app_port); 
+	console.log("USER: " + socket.id + " disconnected from PORT: " + tv_app_port); 
   });
 });
 
@@ -42,16 +43,17 @@ an_io.on('connection', (socket) => {
   
   socket.on('chat message', (msg) => {
 	  console.log(msg);
-	  gc_socket.emit('chat message', msg);
+	  tv_io.emit('chat message', msg);
   });
+  
   socket.on('disconnect', () => 
   {
 	console.log("USER: " + socket.id + " disconnected from PORT: " + an_app_port);
   });
 });
 
-gc_server.listen(gc_app_port, () => {
-  console.log("Listening on port " + gc_app_port);
+tv_server.listen(tv_app_port, () => {
+  console.log("Listening on port " + tv_app_port);
 });
 
 an_server.listen(an_app_port, () => {
