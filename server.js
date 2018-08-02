f3cf0c8b4c612c7330e27f29a9e5a909042fdf18
Wike <stephen.wike@terumobct.com>
2018-08-02 16:37:34 -0600

    Server socket wildcard passes event straight through app and kiosk sockets

--- server.js
+++ server.js
@@ -14,48 +14,97 @@ var tv_socket;
 
 const ROOT = "/var/www/";
 const GAME = "GameCentral/";
+var loginCount = 0;
 
 // Middleware
 //tv_app.use(express.static(path.join(ROOT, GAME, 'HOME')));
 
 tv_app.get('/', (req, res) => {
   console.log("Connected from TV");
-  res.sendFile(__dirname + '/Games/TestChat/index.html');
+  res.sendFile(__dirname + '/root/index.html');
 });
 
 an_app.get('/', (req, res) => {
   console.log("Connected from ANDROID");
+  res.sendFile(__dirname + '/root/testApp.html');
 });
 
-tv_io.on('connection', (socket) => {
-  console.log("CONNECTION TO PORT: " + tv_app_port + ", USER: " + socket.id); 
-  tv_socket = socket;
-  console.log("tv socket: " + socket);
-  
-  socket.on('disconnect', () => 
-  {
-	console.log("USER: " + socket.id + " disconnected from PORT: " + tv_app_port); 
-  });
+tv_io.on('connection', (socket) => 
+{	
+	console.log("TV connected, id: " + socket.id);
+	
+	// Start listening for android apps
+	an_server.listen(an_app_port, () => {
+		console.log("Listening on port " + an_app_port);
+	});
+	
+	socket.on('disconnect', (args) =>
+	{
+		// Close server for android app until tv reconnects
+		console.log("TV disconnected, id: " + socket.id);
+		//an_server.close();
+		//console.log("Closing app server");
+	});
+	
+	// Forward all events to android(s)
+	var onevent = socket.onevent;
+	socket.onevent = function (packet) 
+	{
+		var args = packet.data || [];
+		onevent.call (this, packet);    // original call
+		packet.data = ["*"].concat(args);
+		onevent.call(this, packet);      // additional call to catch-all
+	};
+	
+	socket.on('*', (evt, data) =>
+	{
+		console.log("EVENT " + evt + ", DATA: "); 
+		console.log(data);
+		if (data["id"] == "*")
+		{
+			an_io.emit(evt, data);
+		}
+		else
+		{
+			an_io.to(data["id"]).emit(evt, data);
+		}
+	});
 });
 
-an_io.on('connection', (socket) => {
-  console.log("CONNECTION TO PORT: " + an_app_port + ", USER: " + socket.id); 
-  
-  socket.on('chat message', (msg) => {
-	  console.log(msg);
-	  tv_io.emit('chat message', msg);
-  });
-  
-  socket.on('disconnect', () => 
-  {
-	console.log("USER: " + socket.id + " disconnected from PORT: " + an_app_port);
-  });
+an_io.on('connection', (socket) => 
+{
+	console.log("App connected, id: " + socket.id);
+	loginCount++;
+	var args = { "count": loginCount };
+	tv_io.emit('newconnect', args);
+	
+	socket.on('disconnect', (args) =>
+	{
+		loginCount--;
+		args = { "count": loginCount };
+		console.log("App disconnected, id: " + socket.id);
+		tv_io.emit('disconnect', args);
+	});
+	
+	// Forward all events to tv
+	var onevent = socket.onevent;
+	socket.onevent = function (packet) 
+	{
+		var args = packet.data || [];
+		onevent.call (this, packet);    // original call
+		packet.data = ["*"].concat(args);
+		onevent.call(this, packet);      // additional call to catch-all
+	};
+	
+	socket.on('*', (evt, data) =>
+	{
+		console.log("EVENT " + evt + ", DATA: "); 
+		console.log(data);
+		tv_io.emit(evt, data);
+	});
 });
 
 tv_server.listen(tv_app_port, () => {
   console.log("Listening on port " + tv_app_port);
 });
 
-an_server.listen(an_app_port, () => {
-  console.log("Listening on port " + an_app_port);
-});
